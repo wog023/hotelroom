cddd18c6954d5b2816975773d981f5de5c4ed168
wog023 <60280361+wog023@users.noreply.github.com>
2020-06-29 23:52:10 +0800

    6/29 1152

--- js.js
+++ js.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
 
+	var order1 = document.getElementById('order1')
+	var order2 = document.getElementById('order2')
 
+	function dot(){
+		order1.setAttribute("class","order" );
+		order2.setAttribute("class","order" );
+		order3.setAttribute("class","order" );
+		order4.setAttribute("class","order" );
 
-
-
-
-	
+	}
+	order1.onclick = function(){
+		dot()
+		this.setAttribute("class","order orderNow" )
+	}
+	order2.onclick = function(){
+		dot()
+		this.setAttribute("class","order orderNow" )
+	}
+	order3.onclick = function(){
+		dot()
+		this.setAttribute("class","order orderNow" )
+	}
+	order4.onclick = function(){
+		dot()
+		this.setAttribute("class","order orderNow" )
+	}
 })
